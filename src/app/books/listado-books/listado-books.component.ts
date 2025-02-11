8621fb700b094c23424153bd330a3e9a9afc9c33
ramon05 <ramoncastillo051991@gmail.com>
2025-02-11 19:16:23 -0400

    se creo componente listado-books, interfaz Book y service Book

---
+++ src/app/books/listado-books/listado-books.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-books',
+  standalone: true,
+  imports: [],
+  templateUrl: './listado-books.component.html',
+  styleUrl: './listado-books.component.css'
+})
+export class ListadoBooksComponent {
+
+}
